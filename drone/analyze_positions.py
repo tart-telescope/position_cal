0666f0baf4632ef79cb041f6400d732e70b51c2b
tmolteno <tim@molteno.net>
2024-02-17 22:16:50 +1300

    Get permutations correct

--- drone/analyze_positions.py
+++ drone/analyze_positions.py
@@ -6,6 +6,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def get_angle(p):
+    # make sure that zero degrees is at x=0, y=1 i.e., along the y axis (geographic)
+    return np.degrees(np.arctan2(p[1], p[0])-np.pi/2)
+
+
 def rotate(point, degrees):
     """
     Rotate a point counterclockwise by a given angle around a given origin.
@@ -56,13 +61,30 @@ def best_permute(v, ref):
     return ret
 
 
-def plot_pos(dr, orig):
-    plt.plot(dr[:,0], dr[:,1], 'x', label='drone')
-    plt.plot(orig[:,0], orig[:,1], '.', label='original')
-    # for p in dr:
-    #     plt.circle()
-    plt.grid(True)
-    plt.legend()
+def plot_array(ax, dr, label):
+    ax.plot(dr[:,0], dr[:,1], 'x', label=label)
+
+    x_array, y_array = dr[:,0], dr[:,1]
+
+    i = 0
+    for x,y in zip(x_array, y_array):
+        circle = plt.Circle((x, y), radius=0.15/2,  color='k')
+        ax.add_patch(circle,)
+        ax.text(x,y, f"{i}", color="#FFFFFF", horizontalalignment='center', verticalalignment='center')
+        i = i+1
+
+def plot_pos(dr, orig, labels=["drone", "original"]):
+
+    fig = plt.figure(figsize=(18,6))
+    ax = fig.add_subplot(1,2,1, adjustable='box', aspect=1)
+    plot_array(ax, dr, label=labels[0])
+    ax.grid(True)
+    ax.legend()
+
+    ax2 = fig.add_subplot(1,2,2, adjustable='box', aspect=1)
+    plot_array(ax2, orig, label=labels[1])
+    ax2.grid(True)
+    ax2.legend()
     plt.show()
 
 if __name__=="__main__":
@@ -105,7 +127,7 @@ if __name__=="__main__":
     )
 
     ARGS = parser.parse_args()
-    
+
     orient_ant, orient_degrees = ARGS.orientation
     orient_ant = int(orient_ant)
     orient_degrees = float(orient_degrees)
@@ -177,41 +199,48 @@ if __name__=="__main__":
     print(f"Translation between drone and original = {translate} m")
     plot_pos(dr=drone, orig=original_rt)
 
-    # Now find the permutation that matches best the rotated original to the drone
-    p_best = best_permute(original_rt, drone)
-    
-    print(p_best)
-    print(original_rt[0])
-    print(drone[p_best[0]])
-    
     # Now reconstruct in the original frame of reference, the drone measurements
     drone_r = r_all(drone, -angle)
     drone_rt = translate_all(drone_r, -translate)
 
+    plot_pos(dr=drone_rt, orig=original, labels=['drone_rt', 'original'])
+
+    # Now find the permutation that matches best the rotated original to the drone
+    p_best = best_permute(original, drone_rt)
+
+    print(p_best)
+    print(original[0])
+    print(drone_rt[p_best[0]])
+
+    drone_permuted = ([list(drone_rt[i]).copy() for i in p_best])
+
+    plot_pos(dr=np.array(drone_permuted), orig=original, labels=['drone_permuted', 'original'])
+
+    residuals = [np.sqrt(r_squared(p, o)) for p,o in zip(drone_permuted, original)]
+
     # Global Orientation 
     print(f"Global orientation ant[{orient_ant}] = {orient_degrees} deg")
-    ref_p = drone_rt[orient_ant]
-    actual_angle = np.degrees(np.arctan2(ref_p[1], ref_p[0]))
-    print(f"Actual angle drone_rt[{orient_ant}] = {actual_angle} deg")
-    
+    ref_p = drone_permuted[orient_ant]
+    actual_angle = get_angle(ref_p)
+    print(f"Actual angle original[{orient_ant}] = {actual_angle} deg")
+
     delta_theta = orient_degrees - actual_angle
-    drone_rt = r_all(drone_rt, delta_theta)
+    print(f"Rotating by {delta_theta} degrees")
+    drone_rotated = r_all(drone_permuted, delta_theta)
 
-    ref_p = drone_rt[orient_ant]
-    actual_angle = np.degrees(np.arctan2(ref_p[1], ref_p[0]))
-    print(f"Actual angle drone_rt[{orient_ant}] = {actual_angle} deg")
+    ref_p = drone_rotated[orient_ant]
+    actual_angle = get_angle(ref_p)
+    print(f"Actual angle drone_rotated[{orient_ant}] = {actual_angle} deg")
 
-    final = ([list(drone_rt[p]) for p in p_best])
-    print(final)
+    final = ([list(p) for p in drone_rotated])
 
-    residuals = [np.sqrt(r_squared(p, o)) for p,o in zip(final, original)]
     out_json = {"antenna_positions": [p + [0.0] for p in final],
                 "residuals": residuals,
                 "pixels_per_meter": ARGS.pixels_per_meter}
-    
+
     with open(ARGS.outfile, "w") as fp:
         json.dump(out_json, fp, indent=4, separators=(",", ": "))
 
     final = np.array(final)
-    plot_pos(dr=final, orig=original)
+    plot_pos(dr=final, orig=original, labels=['final', 'original'])
     plt.savefig("matched_drone_pos.png")
